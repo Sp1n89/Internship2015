226ecd23359937e4fbe3704acd6a901cfbe47c0a
Sp1n89 <s.snake90@hotmail.com>
2015-08-01 02:07:31 +0300

    Implemented strategy pattern example

---
+++ Internship2015/DesignPatterns.Strategy/BubbleSort.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DesignPatterns.Strategy
+{
+    public class BubbleSort : ISortStrategy
+    {
+        public void Sort(List<string> list)
+        {
+            bool swapped;
+            do
+            {
+                swapped = false;
+
+                for (var i = 0; i < list.Count - 1; i++)
+                {
+                    var result = String.Compare(list[i], list[i + 1], StringComparison.InvariantCulture);
+                    if (result > 0)
+                    {
+                        var temp = list[i];
+                        list[i] = list[i + 1];
+                        list[i + 1] = temp;
+                        swapped = true;
+                    }
+                }
+            }
+            while (swapped);
+
+            Console.WriteLine("Bubble sorted.");
+        }
+    }
+}
---
+++ Internship2015/DesignPatterns.Strategy/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace DesignPatterns.Strategy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sortedList = new SortedList();
+            sortedList.Add("John Doe");
+            sortedList.Add("Tommy Toe");
+            sortedList.Add("John Smith");
+
+            sortedList.SetSortStrategy(new BubbleSort());
+            sortedList.Sort();
+
+            Console.WriteLine(sortedList);
+
+            Console.ReadLine();
+        }
+    }
+}
