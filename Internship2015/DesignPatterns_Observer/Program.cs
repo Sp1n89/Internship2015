a98d98997ce286579278a21acfce50a2fbfa674b
Sp1n89 <s.snake90@hotmail.com>
2015-08-01 00:46:55 +0300

    Added observer pattern example

---
+++ Internship2015/DesignPatterns_Observer/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace DesignPatterns.Observer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var subject = new Subject();
+            var ob1 = new Observer("Observer_1", subject);
+            var ob2 = new Observer("Observer_2", subject);
+
+            subject.Attach(ob1);
+            subject.Attach(ob2);
+
+            subject.State = "New blog";
+            
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Internship2015/DesignPatterns_Observer/Subject.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+
+namespace DesignPatterns.Observer
+{
+    public class Subject
+    {
+        private readonly IList<IObserver> _observers;
+
+        private string _state;
+        public string State
+        {
+            get { return _state; }
+            set
+            {
+                _state = value;
+                Notify();
+            }
+        }
+
+        public Subject()
+        {
+            _observers = new List<IObserver>();
+        }
+
+        public void Attach(IObserver observer)
+        {
+            _observers.Add(observer);
+        }
+
+        public void Detach(IObserver observer)
+        {
+            _observers.Remove(observer);
+        }
+
+        private void Notify()
+        {
+            foreach (var observer in _observers)
+            {
+                observer.Update("Updated");
+            }
+        }
+    }
+}
