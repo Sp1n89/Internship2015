e9ad6fb3953454ccd6b6aea40bb8091148d4a4c9
Sp1n89 <s.snake90@hotmail.com>
2015-08-01 05:40:24 +0300

    Added template method pattern example

---
+++ Internship2015/DesignPatterns.TemplateMethod/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace DesignPatterns.TemplateMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Device mobile = new MobilePhone();
+            Device laptop = new Laptop();
+
+            mobile.SendEmail("client_1@mail.com", "client_2@mail.com", "Hello", "How are you?");
+            laptop.SendEmail("client_2@mail.com", "client_1@mail.com", "Good", "Good. Thank you!");
+
+            Console.ReadLine();
+        }
+    }
+}
