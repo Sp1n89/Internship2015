7378dee680ed71cd6e3f43f28ea418926cb26023
Sp1n89 <s.snake90@hotmail.com>
2015-08-25 17:48:14 +0300

    fixes

---
+++ Internship2015/QueryOverExample.Domain.Model/Rows/ClientWithOrdersCountsRow.cs
@@ -0,0 +1,18 @@
+﻿namespace QueryOverExample.Domain.Model.Rows
+{
+    public class ClientWithOrdersCountsRow
+    {
+        public int ClientId { get; set; }
+
+        public string Firstname { get; set; }
+
+        public string Lastname { get; set; }
+
+        public int OrdersCount { get; set; }
+
+        public override string ToString()
+        {
+            return string.Format("{0, -15} {1, -15} {2, -15} {3, -15}", ClientId, Firstname, Lastname, OrdersCount);
+        }
+    }
+}
---
+++ Internship2015/QueryOverExample.Presentation/CollectionsHelper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace QueryOverExample.Presentation
+{
+    public static class CollectionsHelper
+    {
+        public static void PrintToConsole<T>(this IEnumerable<T> list) where T : class
+        {
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
